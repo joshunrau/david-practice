54b624c49eeec76b09f3de316e5529a516c3d198
david roper <davidroper321@gmail.com>
2023-09-18 17:33:36 -0400

    sign up notification

--- src/features/auth/pages/SignUpPage.tsx
+++ src/features/auth/pages/SignUpPage.tsx
@@ -1,13 +1,16 @@
 import { CredentialsForm } from '../../../components/CredentialsForm';
 import { UsersDatabase} from '../../../utils/users-database';
+import { useNotificationsStore } from '@douglasneuroinformatics/ui';
 
 export const SignUpPage = () => {
+  const notification = useNotificationsStore();
   return (
     <div className="mx-auto mt-10" style={{ width: 350 }}>
       <CredentialsForm
         label="Sign up"
         onSubmit={(user) => {
-        UsersDatabase.createUser(user);
+        UsersDatabase.createUser(user),
+        notification.addNotification({type:'success', message: 'account created'})
         }}
       />
     </div>
